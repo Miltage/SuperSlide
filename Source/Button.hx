9a77aacdc5ea2169530efe39b2c35b53b21fc0db
Robert Shenton <theshenton@gmail.com>
2016-07-09 14:34:58 +0200

    Add Button class

---
+++ Source/Button.hx
@@ -0,0 +1,39 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.display.MouseEvent;
+
+class Button extends Sprite {
+
+  private var onClick:Void->Void;
+  
+  public function new(onClick:Void->Void)
+  {
+    this.onClick = onClick;
+    
+    addEventListener(MouseEvent.MOUSE_OVER, onMouseOver);
+    addEventListener(MouseEvent.MOUSE_OUT, onMouseOut);
+    addEventListener(MouseEvent.MOUSE_DOWN, onMouseDown);
+    addEventListener(MouseEvent.MOUSE_UP, onMouseUp);
+  }
+
+  public function onMouseOver(m:MouseEvent):Void
+  {
+
+  }
+
+  public function onMouseOut(m:MouseEvent):Void
+  {
+
+  }
+
+  public function onMouseDown(m:MouseEvent):Void
+  {
+
+  }
+
+  public function onMouseUp(m:MouseEvent):Void
+  {
+
+  }
+}
